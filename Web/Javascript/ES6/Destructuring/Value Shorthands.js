cbe997e3ca6cebde1a819d0b86e459a53927d139
meaningchoi <75539452+JiyuChoi@users.noreply.github.com>
2021-04-12 22:34:43 +0900

    Create Value Shorthands.js

---
+++ Web/Javascript/ES6/Destructuring/Value Shorthands.js
@@ -0,0 +1,10 @@
+const follow = checkFollow();
+const alert = checkAlert();
+
+// key 값과 변수명이 같은 경우 생략 가능!
+const settings = {
+    notifications: {
+        flollow, //follow = follow
+        alert //alert = alert
+    }
+};
